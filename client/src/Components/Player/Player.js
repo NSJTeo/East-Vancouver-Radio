3256ecedcec54d730fd597056d061688d3793e83
NSJTeo <n.teomacdonald@gmail.com>
2021-09-20 08:52:25 -0700

    Player JSX

--- client/src/Components/Player/Player.js
+++ client/src/Components/Player/Player.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, createRef } from "react";
 
 export default function Player() {
+  const [muted, setMuted] = useState(false);
+  const [play, setPlay] = useState(true);
+  const playerRef = createRef();
+
+  const toggleMute = () => {
+    setMuted(!muted);
+    // playerRef.current.play();
+  };
+
+  const playAudio = () => {
+    playerRef.current.play();
+    setPlay(false);
+  };
+
   return (
     <>
+      {muted ? <p>Muted</p> : null}
       <audio
-        className="player"
+        ref={playerRef}
         src="http://localhost:8080/stream"
+        type="audio/mp3"
+        muted={muted}
         controls
-        autoplay
+        onPlay={() => console.log("play")}
+        onEnded={() => console.log("ended")}
+        onWaiting={() => setPlay(true)}
+        onPlaying={() => console.log("playing")}
       />
+      <button type="button" onClick={toggleMute}>
+        Mute
+      </button>
+      {play ? (
+        <button type="button" onClick={playAudio}>
+          Start Listening
+        </button>
+      ) : null}
     </>
   );
 }
