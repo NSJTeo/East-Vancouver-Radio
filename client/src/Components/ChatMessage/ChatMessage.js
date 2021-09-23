96ad4980f3761a43834470e89a8e244f27505927
NSJTeo <n.teomacdonald@gmail.com>
2021-09-22 17:44:17 -0700

    Update chat timestamps and display

--- client/src/Components/ChatMessage/ChatMessage.js
+++ client/src/Components/ChatMessage/ChatMessage.js
@@ -2,9 +2,23 @@ import React from "react";
 
 export default function ChatMessage(props) {
   const { body, name, timestamp } = props;
+
+  const commentTime = new Date(timestamp);
+  let hour = commentTime.getHours().toString();
+  if (hour.length === 1) {
+    hour = "0" + hour;
+  }
+  let minutes = commentTime.getMinutes().toString();
+  if (minutes.length === 1) {
+    minutes = "0" + minutes;
+  }
+  console.log(hour.length);
   return (
     <div>
-      {/* render timestamp in a readable manner */}({timestamp}) {name}: {body}
+      <p>
+        ({hour}:{minutes}) {name} says:
+      </p>
+      <p>{body}</p>
     </div>
   );
 }
