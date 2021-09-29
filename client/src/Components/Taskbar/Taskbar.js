e0f943dd63470c2826aaf2980770be31d57c26a3
NSJTeo <n.teomacdonald@gmail.com>
2021-09-28 21:02:09 -0700

    Update taskbar component and styles

--- client/src/Components/Taskbar/Taskbar.js
+++ client/src/Components/Taskbar/Taskbar.js
@@ -3,15 +3,18 @@ import playerIcon from "../../assets/icons/wm-5.png";
 import chatIcon from "../../assets/icons/chat-icon2.png";
 import startIcon from "../../assets/icons/start-button.png";
 import setupIcon from "../../assets/icons/setupIcon.png";
+import aboutIcon from "../../assets/icons/help_book_cool-4.png";
 
 export default function Taskbar(props) {
   const {
     handlePlayerIconClick,
     handleChatIconClick,
     handleSetupIconClick,
+    handleAboutIconClick,
     playerOn,
     chatOn,
     setupOn,
+    aboutOn,
   } = props;
   return (
     <div className="taskbar">
@@ -66,6 +69,22 @@ export default function Taskbar(props) {
           alt="Globe on a folder"
         />
       </button>
+      <button
+        className={
+          aboutOn
+            ? "taskbar__button taskbar__button--active"
+            : "taskbar__button taskbar__button--inactive"
+        }
+        onClick={() => handleAboutIconClick()}
+      >
+        <p className="taskbar__button-name">About</p>
+        <img className="taskbar__button-icon" src={aboutIcon} alt="..." />
+      </button>
+      <div className="taskbar__name-container">
+        <p className="taskbar__name">
+          East Vancouver Broadcasting Corporation 2021
+        </p>
+      </div>
     </div>
   );
 }
