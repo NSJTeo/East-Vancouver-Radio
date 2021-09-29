a8f78a123eb3da1163fb78805315812e614bce90
NSJTeo <n.teomacdonald@gmail.com>
2021-09-28 21:04:26 -0700

    Update app.js structure

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import { useState } from "react";
 import Taskbar from "./Components/Taskbar/Taskbar";
 import Desktop from "./Components/Desktop/Desktop";
 import Setup from "./Components/Setup/Setup";
+import About from "./Components/About/About";
 
 function App() {
   const [playerOn, setPlayerOn] = useState(false);
   const [chatOn, setChatOn] = useState(false);
   const [setupOn, setSetupOn] = useState(false);
+  const [aboutOn, setAboutOn] = useState(false);
   const [activeWindow, setActiveWindow] = useState("");
 
   const setPlayerToActive = () => {
@@ -24,6 +26,10 @@ function App() {
     setActiveWindow("setup");
   };
 
+  const setAboutToActive = () => {
+    setActiveWindow("about");
+  };
+
   const handlePlayerIconClick = () => {
     setPlayerToActive();
     setPlayerOn(!playerOn);
@@ -36,12 +42,17 @@ function App() {
     setSetupToActive();
     setSetupOn(!setupOn);
   };
+  const handleAboutIconClick = () => {
+    setAboutToActive();
+    setAboutOn(!aboutOn);
+  };
   return (
     <div className="app-container">
       <Desktop
         handlePlayerIconClick={handlePlayerIconClick}
         handleChatIconClick={handleChatIconClick}
         handleSetupIconClick={handleSetupIconClick}
+        handleAboutIconClick={handleAboutIconClick}
       />
       <Player
         playerOn={playerOn}
@@ -55,6 +66,12 @@ function App() {
         setChatToActive={setChatToActive}
         activeWindow={activeWindow}
       />
+      <About
+        aboutOn={aboutOn}
+        handleAboutIconClick={handleAboutIconClick}
+        setAboutToActive={setAboutToActive}
+        activeWindow={activeWindow}
+      />
       <Setup
         setupOn={setupOn}
         handleSetupIconClick={handleSetupIconClick}
@@ -65,9 +82,11 @@ function App() {
         handlePlayerIconClick={handlePlayerIconClick}
         handleChatIconClick={handleChatIconClick}
         handleSetupIconClick={handleSetupIconClick}
+        handleAboutIconClick={handleAboutIconClick}
         playerOn={playerOn}
         chatOn={chatOn}
         setupOn={setupOn}
+        aboutOn={aboutOn}
       />
     </div>
   );
